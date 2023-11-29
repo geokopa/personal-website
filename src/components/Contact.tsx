a762f1d136f60c4a3c1f196a0eeaf2fed8ede55a
George Kopadze <kopadze@gmail.com>
2023-11-29 16:15:27 -0500

    fix the name attribute on div

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,7 @@
 const Contact = () => {
   return (
     <div
-      name="contact"
+      id="contact"
       className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 "
     >
       <form action="" className="flex flex-col max-w-[600px] w-full">
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -9,7 +9,7 @@ import JavaScript from "../assets/js.png";
 
 export const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f]  text-gray-300 ">
+    <div id="skills" className="w-full h-screen bg-[#0a192f]  text-gray-300 ">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full ">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 py-4">
