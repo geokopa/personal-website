45a1fa37ceb5974ffee0d647ace0c32236ff78e9
George Kopadze <kopadze@gmail.com>
2023-12-05 14:29:01 -0500

    update references

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { About } from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
 import Navbar from "./components/shared/Navbar";
-import Skills from "./components/pages/Skills";
+// import Skills from "./components/pages/Skills";
 import Testimonials from "./components/pages/Testimonial";
 import Clients from "./components/pages/Clients";
 import Timeline from "./components/pages/Timeline";
