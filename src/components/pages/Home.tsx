caec3042c265e111be0fbe18bcf29ec054696775
George Kopadze <kopadze@gmail.com>
2023-12-08 23:47:23 -0500

    Included photo on homepage and fixed timeline overflow issue

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
+import ProfilePhoto from "../../assets/images/profile/me.svg";
 const Home = () => {
   return (
     <div id="home" className="w-full h-screen bg-[#0a192f]">
@@ -8,12 +9,23 @@ const Home = () => {
         <p className="text-pink-600 text-3xl sm:text-4xl pb-3">
           Hi, my name is
         </p>
-        <h1 className="text-3xl sm:text-7xl font-bold text-[#ccd6f6]">
-          George Kopadze
-        </h1>
-        <h2 className="text-3xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Full Stack Developer.
-        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-1">
+          <div className="sm:col-span-2">
+            <h1 className="text-3xl sm:text-7xl font-bold text-[#ccd6f6]">
+              George Kopadze
+            </h1>
+            <h2 className="text-3xl sm:text-7xl font-bold text-[#8892b0]">
+              I'm a Full Stack Developer.
+            </h2>
+          </div>
+          <div className="sm:col-span-1">
+            <img
+              src={ProfilePhoto}
+              alt="profile"
+              className="w-32 md:w-56 rounded-full shadow-md hidden lg:block"
+            />
+          </div>
+        </div>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           If you're seeking a results-driven software engineer with a proven
           track record of delivering innovative software solutions using C# and
--- src/components/pages/Timeline.tsx
+++ src/components/pages/Timeline.tsx
@@ -12,39 +12,41 @@ const Timeline = () => {
           <p className="py-8"></p>
         </div>
 
-        <ol className="relative border-s border-gray-200 dark:border-gray-700  bg-[#0a192f] p-6">
-          {ExperienceList.map((client: ExperienceItem) => {
-            return (
-              <li className="mb-8 ms-6" key={client.id}>
-                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                  <svg
-                    className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                  </svg>
-                </span>
-                <h3 className="flex items-center mb-1 text-md sm:text-md md:text-lg font-semibold text-pink-500">
-                  {client.name} - {client.position}{" "}
-                  {client.id === 1 && (
-                    <span className="bg-blue-100 text-blue-800 text-xs sm:text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">
-                      Latest
-                    </span>
-                  )}
-                </h3>
-                <time className="block mb-2 text-sm sm:text-md font-normal leading-none text-gray-400 dark:text-gray-500">
-                  {client.workYears}
-                </time>
-                <p className="mb-4 text-xs sm:text-sm font-normal text-gray-500 dark:text-gray-400">
-                  {client.description}
-                </p>
-              </li>
-            );
-          })}
-        </ol>
+        <div className="p-6 sm:p-4 overflow-y-auto">
+          <ol className="relative border-s border-gray-200 dark:border-gray-700 bg-[#0a192f] p-6 sm:p-4">
+            {ExperienceList.map((client: ExperienceItem) => {
+              return (
+                <li className="mb-8 ms-2" key={client.id}>
+                  <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900 ml-1 sm:ml-0">
+                    <svg
+                      className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                    </svg>
+                  </span>
+                  <h3 className="flex items-center mb-1 text-md sm:text-md md:text-lg font-semibold text-pink-500">
+                    {client.name} - {client.position}{" "}
+                    {client.id === 1 && (
+                      <span className="bg-blue-100 text-blue-800 text-xs sm:text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">
+                        Latest
+                      </span>
+                    )}
+                  </h3>
+                  <time className="block mb-2 text-sm sm:text-md font-normal leading-none text-gray-400 dark:text-gray-500">
+                    {client.workYears}
+                  </time>
+                  <p className="mb-4 text-xs sm:text-sm font-normal text-gray-500 dark:text-gray-400">
+                    {client.description}
+                  </p>
+                </li>
+              );
+            })}
+          </ol>
+        </div>
       </div>
     </div>
   );
